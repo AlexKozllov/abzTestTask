5ae7921cc5a963fe5fd70770353859e0fdbce162
Alex Kozlov <o.kozlov@raccoon.pet>
2024-05-24 12:15:12 +0300

    ADD: Added tooltiptext for User

--- src/components/successfullyRegistered/SuccessfullyRegistered.js
+++ src/components/successfullyRegistered/SuccessfullyRegistered.js
@@ -5,7 +5,7 @@ import s from './successfullyRegistered.module.scss';
 
 const SuccessfullyRegistered = () => {
     return (
-        <section>
+        <section className={s.wrapper}>
             <h2>User successfully registered</h2>
             <svg className={s.logoImg}>
                 <use href={sprite + '#success-image'} />
